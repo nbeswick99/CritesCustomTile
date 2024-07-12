aa038e6739f4124cadd1baad5db47395f2d87324
nbeswick99 <n.beswick1999@gmail.com>
2024-07-12 19:21:42 -0400

    little more home page. contact form css

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -3,18 +3,20 @@ import {useState} from 'react'
 import axios from 'axios'
 import {useNavigate} from 'react-router-dom' 
 
+import '../css/ContactForm.css'
+
 const ContactForm = () => {
   const [contact, setContact] = useState({
     firstName: "", 
     lastName: "",
     email: "",
-    number: []
+    number: "",
   });
 
   const Navigate = useNavigate();
 
   const changeHandler = (e) => {
-    setProduct((prevState) => ({...prevState,
+    setContact((prevState) => ({...prevState,
       [e.target.name]: e.target.value
       }))
     };
@@ -40,21 +42,22 @@ const ContactForm = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={e => submitHandler(e)}>
-        <div>
+    <div className='contact-form-container'>
+      <h1>Contact</h1>
+      <form onSubmit={e => submitHandler(e)} className='form'>
+        <div className='input-container'>
           <label htmlFor="firstName">First Name</label>
           <input type="text" id="firstName" name="firstName" value={contact.firstName} onChange ={e => changeHandler(e)}/>
         </div>
-        <div>
+        <div className='input-container'>
           <label htmlFor="lastName">Last Name</label>
           <input type="text" id="lastName" name="lastName" value={contact.lastName} onChange ={e => changeHandler(e)}/>
         </div>
-        <div>
+        <div className='input-container'>
           <label htmlFor="email">Email</label>
           <input type="text" id="email" name="email" value={contact.email} onChange ={e => changeHandler(e)}/>
         </div>
-        <div>
+        <div className='input-container'>
           <label htmlFor="number">Number</label>
           <input type="text" id="number" name="number" value={contact.number} onChange ={e => changeHandler(e)}/>
         </div>
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
+//Component import
+import ContactForm from '../components/ContactForm';
+//css import 
+import '../css/Home.css'
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      {/* Div with hero image and contact form */}
+      <div className='hero-container'>
+        <div className='hero-text-container'>
+          <h2 className='hero-text'>Crites Custom Tile</h2>
+          <p className='hero-text'>Serving Jackson County, IN - Bartholomew County, IN - Scottsburg, IN - Austin, IN</p>
+        </div>
+        <ContactForm />
+      </div>
     </div>
   )
 }
