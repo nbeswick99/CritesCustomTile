fba34f6543407ae4d5df81559fd791272071b84c
nbeswick99 <n.beswick1999@gmail.com>
2024-08-08 05:25:45 -0400

    Changes on the Contact card and AdminHome

--- client/src/components/ContactCard.jsx
+++ client/src/components/ContactCard.jsx
@@ -1,29 +1,39 @@
 import React from "react";
+import axios from "axios";
+// css imports
+import '../css/ContactCard.css'
 
-// import axios from "axios";
 const ContactCard = (props) => {
-    const contact = props.contact
+    const {contact, deleteOne} = props;
 
     const formatPhone = (number) => {
         let phone = number
         return `+${phone.slice(0, 1)} (${phone.slice(1,4)}) ${phone.slice(4,7)} - ${phone.slice(7, phone.length)}`
     }
 
+    
+
+
     return ( contact?(
-        <div>
-            <div className="info-container">
-                <p>Name: </p>
-                <p>{contact.firstName + " " + contact.lastName}</p>
-            </div>
-            <div className="info-container">
-                <p>Email: </p>
-                <p>{contact.email}</p>
-            </div>55
+        <div className="contact-card">
             <div>
                 <div className="info-container">
-                    <p>Number: </p>
-                    <p>{formatPhone(contact.number)}</p>
+                    <p>Name: </p>
+                    <p>{contact.firstName + " " + contact.lastName}</p>
                 </div>
+                <div className="info-container">
+                    <p>Email: </p>
+                    <p>{contact.email}</p>
+                </div>
+                <div>
+                    <div className="info-container">
+                        <p>Number: </p>
+                        <p>{formatPhone(contact.number)}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="button-container">
+                <button className="delete-button" onClick={() => deleteOne(contact._id)}> Delete </button>
             </div>
         </div>
     ): <h1>Loading</h1>
--- client/src/views/AdminHome.jsx
+++ client/src/views/AdminHome.jsx
@@ -5,21 +5,42 @@ import { useNavigate} from "react-router-dom"
 import ContactCard from "../components/ContactCard"
 //css import
 import '../css/AdminHome.css' 
+
 const AdminHome = () => {
+    //state variables
     const [contacts, setContacts] = useState();
-    
+    const [deleteClient, setDeleteClient] = useState(false);
+
+    //make useNavigate easier to use 
     const navigate = useNavigate();
 
+    // Delete to be passed to each contact, stored here so the we can 
+    // update deleteClient variable for useEffect function
+
+    const deleteOne = ((_id) => {
+        axios.delete(`http://localHost:8000/api/clients/${_id}`)
+        .then(res => {
+          console.log(res.data)
+          setDeleteClient(true);
+        })
+        .catch(err => console.log(err))
+      })
+    
+    // deleteClient serves to cause this to reload, getting updated
+    // contact list 
     useEffect(()=>{
         axios.get("http://localhost:8000/api/clients")
             .then((res) => {
               setContacts(res.data);
               console.log(res.data);
+              if (deleteClient == true) {
+                setDeleteClient(false);
+              }
             })
             .catch((err) => {
                 console.log(err); 
             })
-      }, [])
+      }, [deleteClient])
 
     return (
        <div>
@@ -34,7 +55,7 @@ const AdminHome = () => {
                 { contacts? (
                 <div className='contacts'>
                     {contacts.map((eachContact, IDX) => {
-                        return <ContactCard key={IDX} contact={eachContact}/>
+                        return <ContactCard key={IDX} contact={eachContact} deleteOne = {deleteOne}/>
                     })}
                 </div>     
                 ): (<h1> You have no contacts</h1>
